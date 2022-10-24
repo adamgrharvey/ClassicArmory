588392b7aa23a107d8bb0bfdacadfea1fd4c78c3
adamgrharvey <adamgrharvey@gmail.com>
2022-10-23 18:01:31 -0700

    working popup box with characterstring

--- Armory.lua
+++ Armory.lua
@@ -1,6 +1,7 @@
 Armory = {};
 Armory.fully_loaded = false;
-local GetUpdate = true;
+local GetUpdate = false;
+local CharacterString = "";
 local Character = {
 	[0] = "",
 	[1] = "",
@@ -218,9 +219,82 @@ function Armory.UpdateFrame()
 		GetUpdate = false
 		Armory.GetCharData()
 		print('update')
+		EditBox_Show(CharacterString)
 	end
 	
 	Armory.Label:SetText(GetItemInfo(GetInventoryItemLink("player", 1)));
+end
+
+function EditBox_Show(text)
+	if not EditBox then
+			local f = CreateFrame("Frame", "EditBox", UIParent, "DialogBoxFrame")
+			f:SetPoint("CENTER")
+			f:SetSize(600, 500)
+			
+			f:SetBackdrop({
+					bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+					edgeFile = "Interface\\PVPFrame\\UI-Character-PVP-Highlight", -- this one is neat
+					edgeSize = 16,
+					insets = { left = 8, right = 6, top = 8, bottom = 8 },
+			})
+			f:SetBackdropBorderColor(0, .44, .87, 0.5) -- darkblue
+			
+			-- Movable
+			f:SetMovable(true)
+			f:SetClampedToScreen(true)
+			f:SetScript("OnMouseDown", function(self, button)
+					if button == "LeftButton" then
+							self:StartMoving()
+					end
+			end)
+			f:SetScript("OnMouseUp", f.StopMovingOrSizing)
+			
+			-- ScrollFrame
+			local sf = CreateFrame("ScrollFrame", "EditBoxScrollFrame", EditBox, "UIPanelScrollFrameTemplate")
+			sf:SetPoint("LEFT", 16, 0)
+			sf:SetPoint("RIGHT", -32, 0)
+			sf:SetPoint("TOP", 0, -16)
+			sf:SetPoint("BOTTOM", EditBoxButton, "TOP", 0, 0)
+			
+			-- EditBox
+			local eb = CreateFrame("EditBox", "EditBoxEditBox", EditBoxScrollFrame)
+			eb:SetSize(sf:GetSize())
+			eb:SetMultiLine(true)
+			eb:SetAutoFocus(false) -- dont automatically focus
+			eb:SetFontObject("ChatFontNormal")
+			eb:SetScript("OnEscapePressed", function() f:Hide() end)
+			sf:SetScrollChild(eb)
+			
+			-- Resizable
+			f:SetResizable(true)
+			f:SetMinResize(150, 100)
+			
+			local rb = CreateFrame("Button", "EditBoxResizeButton", EditBox)
+			rb:SetPoint("BOTTOMRIGHT", -6, 7)
+			rb:SetSize(16, 16)
+			
+			rb:SetNormalTexture("Interface\\ChatFrame\\UI-ChatIM-SizeGrabber-Up")
+			rb:SetHighlightTexture("Interface\\ChatFrame\\UI-ChatIM-SizeGrabber-Highlight")
+			rb:SetPushedTexture("Interface\\ChatFrame\\UI-ChatIM-SizeGrabber-Down")
+			
+			rb:SetScript("OnMouseDown", function(self, button)
+					if button == "LeftButton" then
+							f:StartSizing("BOTTOMRIGHT")
+							self:GetHighlightTexture():Hide() -- more noticeable
+					end
+			end)
+			rb:SetScript("OnMouseUp", function(self, button)
+					f:StopMovingOrSizing()
+					self:GetHighlightTexture():Show()
+					eb:SetWidth(sf:GetWidth())
+			end)
+			f:Show()
+	end
+	
+	if text then
+			EditBoxEditBox:SetText(text)
+	end
+	EditBox:Show()
 end
 
 function Armory.GetCharData()
@@ -237,18 +311,23 @@ function Armory.GetCharData()
 			local itemString = string.match(itemLink, "item[%-?%d:]+")
 			itemString = string.gsub(itemString, "::", ":0:")
 			itemString = string.gsub(itemString, "::", ":0:")
+			itemString = string.sub(itemString,6,-1)
 			_G.ArmoryPrefs[charUnique].inventory[i] = itemString
+			CharacterString = CharacterString..itemString.."."
 		else
 			_G.ArmoryPrefs[charUnique].inventory[i] = nil;
+			CharacterString = CharacterString.."."
 		end
 	end
+	CharacterString = string.sub(CharacterString,2,-1);
 	local ammoID, _ = GetInventoryItemID("player", 0)
 	if (ammoID > 0) then
 		_G.ArmoryPrefs[charUnique].inventory[0] = "item:"..ammoID
 	else
 		_G.ArmoryPrefs[charUnique].inventory[0] = nil
 	end
-_G.ArmoryPrefs[charUnique].Statistics = Armory.GetStatisticData();
+_G.ArmoryPrefs[charUnique].Statistics = Armory.GetStatisticData()
+_G.ArmoryPrefs[charUnique].CharacterString = CharacterString
 _G.ArmoryPrefs[charUnique].RecentStats = { GetLatestUpdatedStats() }
 _G.ArmoryPrefs[charUnique].RecentAchieves = { GetLatestCompletedAchievements() }
 _G.ArmoryPrefs[charUnique].Specialization = {Primary = "", Secondary = ""}
@@ -309,18 +388,18 @@ function Armory.GetStatisticData()
 
 	for _, Achievement in ipairs(AchievementList) do 
 		IDNumber, Name, Points, Completed, Month, Day, Year, Description, Flags, Image, RewardText, isGuildAch = GetAchievementInfo(Achievement)
-		Statistics[Achievement] = {};
-		Statistics[Achievement].IDNumber = IDNumber
-		Statistics[Achievement].Value = GetStatistic(Achievement)
-		Statistics[Achievement].Name = Name
-		Statistics[Achievement].Description = Description
-		Statistics[Achievement].Points = Points
-		Statistics[Achievement].Completed = Completed
-		Statistics[Achievement].Month = Month
-		Statistics[Achievement].Day = Day
-		Statistics[Achievement].Year = Year
-		Statistics[Achievement].Flags = Flags
-		Statistics[Achievement].RewardText = RewardText
+		local stat = GetStatistic(Achievement)
+		stat = string.gsub(stat, "|TInterface\\MoneyFrame\\UI-GoldIcon:0:0:2:0|t ", "")
+		stat = string.gsub(stat, "|TInterface\\MoneyFrame\\UI-SilverIcon:0:0:2:0|t ", "")
+		stat = string.gsub(stat, "|TInterface\\MoneyFrame\\UI-CopperIcon:0:0:2:0|t", "")
+
+		if (stat ~= "0" or string.sub(stat, 1, 3) ~= "0 /" or stat ~= "--") then
+			Statistics[Achievement] = {};
+			Statistics[Achievement].Value = stat
+			Statistics[Achievement].Month = Month
+			Statistics[Achievement].Day = Day
+			Statistics[Achievement].Year = Year
+		end
 	end
 	return Statistics
 end
